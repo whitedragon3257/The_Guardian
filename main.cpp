845c191f9173247856b17106b3de0c76a0fa870c
WH1T3 DR4GON <162749588+whitedragon3257@users.noreply.github.com>
2024-04-09 10:33:20 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,3 +2,8 @@
 * By: White Dragon
 * Description: This Code is initial point for interation with the user. This is menu for acess the AI.
 */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime> 
